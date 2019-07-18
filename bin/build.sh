b1f0ff92b39f8411ac637909eca5cf9108866d49
振阳 <1633226379@qq.com>
2019-07-18 17:04:27 +0800

    修改自动build脚本

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,8 @@ build() {
 	for d in $(ls ./$1); do
 		echo "building $1/$d"
 		pushd $dir/$1/$d >/dev/null
-		CGO_ENABLED=0 GOOS=linux go build -o cmd -a -installsuffix cgo -ldflags '-w'
+		# CGO_ENABLED=0 GOOS=linux go build -o cmd -a -installsuffix cgo -ldflags '-w'
+		CGO_ENABLED=0 GOOS=linux make proto && make build
 		popd >/dev/null
 	done
 }
